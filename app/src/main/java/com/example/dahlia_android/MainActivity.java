51e921a757c32fa1bd69f84f3f51b96946ddd0ce
TCross <t05401btv@gmail.com>
2021-03-06 14:55:43 -0500

    Finished navigation of top level destinations.

--- app/src/main/java/com/example/dahlia_android/MainActivity.java
+++ app/src/main/java/com/example/dahlia_android/MainActivity.java
@@ -5,7 +5,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.dahlia_android.ui.friends.FriendsFragment;
 import com.example.dahlia_android.ui.groups.GroupsFragment;
+import com.example.dahlia_android.ui.home.HomeFragment;
+import com.example.dahlia_android.ui.messages.MessagesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -40,15 +44,17 @@ public class MainActivity extends AppCompatActivity {
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        BottomNavigationView btm = findViewById(R.id.navigation);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_friends, R.id.nav_groups)
+                R.id.nav_home, R.id.nav_friends, R.id.nav_groups, R.id.nav_messages, R.id.nav_aupair_nearby)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(btm, navController);
     }
 
     @Override
@@ -65,6 +71,63 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+    public void goHome(MenuItem item) {
+        item.setChecked(true);
+        FragmentManager managerHome = getSupportFragmentManager();
+        if(managerHome != null) {
+            FragmentTransaction transaction = managerHome.beginTransaction();
+            if( transaction != null ) {
+                transaction.replace(R.id.drawer_layout,new HomeFragment());
+                transaction.commit();
+            }
+        }
+    }
+
     public void goFriends(MenuItem item) {
+        item.setChecked(true);
+        FragmentManager managerFriends = getSupportFragmentManager();
+        if (managerFriends != null ) {
+            FragmentTransaction transaction = managerFriends.beginTransaction();
+            if( transaction != null ) {
+                transaction.replace(R.id.drawer_layout,new FriendsFragment());
+                transaction.commit();
+            }
+        }
+    }
+
+    public void goGroups(MenuItem item) {
+        item.setChecked(true);
+        FragmentManager managerGroups = getSupportFragmentManager();
+        if (managerGroups != null ) {
+            FragmentTransaction transaction = managerGroups.beginTransaction();
+            if( transaction != null ) {
+                transaction.replace(R.id.drawer_layout,new GroupsFragment());
+                transaction.commit();
+            }
+        }
+    }
+
+    public void goMessages(MenuItem item) {
+        item.setChecked(true);
+        FragmentManager managerMessages = getSupportFragmentManager();
+        if (managerMessages != null ) {
+            FragmentTransaction transaction = managerMessages.beginTransaction();
+            if( transaction != null ) {
+                transaction.replace(R.id.drawer_layout,new MessagesFragment());
+                transaction.commit();
+            }
+        }
+    }
+
+    public void goAuPairNearby(MenuItem item) {
+    }
+
+    public void goRSSFeed(MenuItem item) {
+    }
+
+    public void goAbout(MenuItem item) {
+    }
+
+    public void goSettings(MenuItem item) {
     }
 }
